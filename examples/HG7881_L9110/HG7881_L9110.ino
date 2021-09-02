25421d9640399bc757c7f1b70040c52d7f63e78f
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2021-09-02 20:18:37 +0200

    Add files via upload

---
+++ examples/HG7881_L9110/HG7881_L9110.ino
@@ -0,0 +1,33 @@
+/********************************************************
+ * PID Basic Example
+ 
+ ********************************************************/
+
+#include <Motor_PID.h>
+#define ENCA 2 // YELLOW from polulu
+#define ENCB 3 // WHITE from polulu
+
+#define IN2 11 //B1-A 
+#define IN1 10 //A1-A 
+long prevT;
+int target = 900;
+//int target = 50*sin(prevT/1e6);
+//int target=300;
+//int target=analogRead(A0);
+// PID constants
+float kp = 1;
+float kd = 0.1;
+float ki = 0.02;
+//int target = 50*sin(prevT/1e6);
+motor motor1(ENCA, ENCB, IN1, IN2);
+void setup()
+{
+    motor1.init(kp, kd, ki);
+}
+void loop()
+{
+    // long currT = micros();
+    // float deltaT = ((float)(currT - prevT)) / (1.0e6);
+    //prevT = currT;
+    motor1.start(target);
+}
