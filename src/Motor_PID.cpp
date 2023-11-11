22abac1980de9f0cc6d2ce26ee8bbf97ad3a3217
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2023-11-11 18:54:49 +0100

    fix naming convention

--- src/Motor_PID.cpp
+++ src/Motor_PID.cpp
@@ -1,91 +1,87 @@
 #include "Motor_PID.h"
 
-motor::motor(uint8_t ENCA, uint8_t ENCB, uint8_t IN1, uint8_t IN2, uint8_t pwmpin, int lower_limit, int upper_limit)
+Motor::Motor(uint8_t enca, uint8_t encb, uint8_t in1, uint8_t in2, uint8_t pwmpin, int lower_limit, int upper_limit)
 {
-  this->ENCA = ENCA;
-  this->ENCB = ENCB;
-  this->IN1 = IN1;
-  this->IN2 = IN2;
+  this->enca = enca;
+  this->encb = encb;
+  this->in1 = in1;
+  this->in2 = in2;
   _pwmpin = pwmpin;
   _upper_limit = upper_limit;
   _lower_limit = lower_limit;
 }
-void motor::init(float kp, float ki, float kd)
+
+void Motor::init(float kp, float ki, float kd)
 {
   this->kp = kp;
   this->kd = kd;
   this->ki = ki;
-  pinMode(ENCA, INPUT);
-  pinMode(ENCB, INPUT);
+  pinMode(enca, INPUT);
+  pinMode(encb, INPUT);
   if (_pwmpin != 0)
     pinMode(_pwmpin, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
   motor_state = 1;
 }
-void motor::RisingInterrupt()
+
+void Motor::rising_interrupt()
 {
-  bool reading = digitalRead(ENCA);
+  bool reading = digitalRead(enca);
   if (reading != buffer)
   {
     buffer = reading;
     if (reading)
     {
-      readEncoder();
+      read_encoder();
     }
   }
 }
-void motor::setMotor(int dir, int pwmVal)
+
+void Motor::set_motor(int dir, int pwm_val)
 {
-  pwmVal = constrain(pwmVal, _lower_limit, _upper_limit);
+  pwm_val = constrain(pwm_val, _lower_limit, _upper_limit);
   if (_pwmpin != 0)
-    analogWrite(_pwmpin, pwmVal);
+    analogWrite(_pwmpin, pwm_val);
   if (dir == 1 && motor_state == 1)
   {
-    (_pwmpin != 0) ? digitalWrite(IN1, 1) : analogWrite(IN1, pwmVal);
-    (_pwmpin != 0) ? digitalWrite(IN2, 0) : analogWrite(IN2, 0);
+    (_pwmpin != 0) ? digitalWrite(in1, 1) : analogWrite(in1, pwm_val);
+    (_pwmpin != 0) ? digitalWrite(in2, 0) : analogWrite(in2, 0);
   }
   else if (dir == -1 && motor_state == 1)
   {
-    (_pwmpin != 0) ? digitalWrite(IN1, 0) : analogWrite(IN1, 0);
-    (_pwmpin != 0) ? digitalWrite(IN2, 1) : analogWrite(IN2, pwmVal);
+    (_pwmpin != 0) ? digitalWrite(in1, 0) : analogWrite(in1, 0);
+    (_pwmpin != 0) ? digitalWrite(in2, 1) : analogWrite(in2, pwm_val);
   }
   else
   {
-    (_pwmpin != 0) ? digitalWrite(IN1, 0) : analogWrite(IN1, 0);
-    (_pwmpin != 0) ? digitalWrite(IN1, 0) : analogWrite(IN2, 0);
+    (_pwmpin != 0) ? digitalWrite(in1, 0) : analogWrite(in1, 0);
+    (_pwmpin != 0) ? digitalWrite(in2, 0) : analogWrite(in2, 0);
   }
 }
-void motor::start()
+
+void Motor::start()
 {
+  long curr_t = micros();
+  rising_interrupt();
 
-  long currT = micros();
-  RisingInterrupt();
+  float delta_t = ((float)(curr_t - prev_t)) / (1.0e6);
+  prev_t = curr_t;
 
-  // time difference
-  float deltaT = ((float)(currT - prevT)) / (1.0e6);
-  prevT = currT;
-
-  // error
   long e = posi - target;
 
-  // derivative
-  float dedt = (e - eprev) / (deltaT);
+  float dedt = (e - eprev) / (delta_t);
 
-  // integral
-  eintegral = eintegral + e * deltaT;
+  eintegral = eintegral + e * delta_t;
 
-  // control signal
   float u = kp * e + kd * dedt + ki * eintegral;
 
-  // motor power
   float pwr = fabs(u);
   if (pwr > 255)
   {
     pwr = 255;
   }
 
-  // motor direction
   int dir = 1;
   if (u < 0)
   {
@@ -94,7 +90,6 @@ void motor::start()
   if (e == 0)
   {
     turn_off();
-
     target_is_reached = true;
   }
   else
@@ -103,15 +98,14 @@ void motor::start()
     target_is_reached = false;
   }
 
-  // signal the motor
-  setMotor(dir, pwr);
+  set_motor(dir, pwr);
 
-  // store previous error
   eprev = e;
 }
-void motor::readEncoder()
+
+void Motor::read_encoder()
 {
-  bool b = digitalRead(ENCB);
+  bool b = digitalRead(encb);
   if (b)
   {
     posi++;
@@ -121,42 +115,48 @@ void motor::readEncoder()
     posi--;
   }
 }
-void motor::turn_on()
+
+void Motor::turn_on()
 {
   motor_state = 1;
 }
-void motor::turn_off()
-{
 
+void Motor::turn_off()
+{
   motor_state = 0;
   if (_pwmpin != 0)
     analogWrite(_pwmpin, 0);
-  (_pwmpin != 0) ? digitalWrite(IN1, 0) : analogWrite(IN1, 0);
-  (_pwmpin != 0) ? digitalWrite(IN2, 0) : analogWrite(IN2, 0);
+  (_pwmpin != 0) ? digitalWrite(in1, 0) : analogWrite(in1, 0);
+  (_pwmpin != 0) ? digitalWrite(in2, 0) : analogWrite(in2, 0);
 }
 
-void motor::set_position(float posi)
+void Motor::set_position(float position)
 {
-  this->posi = (long)round(posi);
+  posi = (long)round(position);
 }
-long motor::get_position()
+
+long Motor::get_position()
 {
   return posi;
 }
-void motor::set_target(float target)
+
+void Motor::set_target(float target)
 {
   this->target = (long)round(target);
 }
-long motor::get_target()
+
+long Motor::get_target()
 {
   return target;
 }
-void motor::limit(int lower_limit, int upper_limit)
+
+void Motor::limit(int lower_limit, int upper_limit)
 {
   _lower_limit = lower_limit;
   _upper_limit = upper_limit;
 }
-bool motor::target_reached(bool reset)
+
+bool Motor::target_reached(bool reset)
 {
   if (reset)
     target_is_reached = false;
--- src/Motor_PID.h
+++ src/Motor_PID.h
@@ -1,3 +1,4 @@
+#pragma once
 // author:: Natan :Lisowski
 //  this library let you control motor with pwm signal
 // this works with HG7881 Hbridge  L9110S
@@ -5,22 +6,21 @@
 //
 //  library inspired by https://github.com/curiores/ArduinoTutorials
 //   encoder motor control
-#pragma once
 #include <Arduino.h>
 #include <math.h>
 
-class motor
+class Motor
 {
 public:
-	float kp = 0.0f, kd = 0.0f, ki = 0.0f;																						  // your pid variables
-	bool motor_state{false};																									  // this variable let the pwm signal go
-	motor(uint8_t ENCA, uint8_t ENCB, uint8_t IN1, uint8_t IN2, uint8_t pwmpin = 0, int lower_limit = 50, int upper_limit = 255); // constructor
-	~motor(){};
-	void start();							 // place this in loop without delays
+	float kp = 0.0f, kd = 0.0f, ki = 0.0f;  // your pid variables
+	bool motor_state{ false };                 // this variable let the pwm signal go
+	Motor(uint8_t enca, uint8_t encb, uint8_t in1, uint8_t in2, uint8_t pwmpin = 0, int lower_limit = 50, int upper_limit = 255); // constructor
+	~Motor() {};
+	void start();           // place this in loop without delays
 	void init(float kp, float ki, float kd); // this function initializes pid regulator
-	void turn_on();							 // changes motor_state variable to true
-	void turn_off();						 // changes motor_state variable to false
-	long posi{0};							 // position of rotary encoder \ number of pulses
+	void turn_on();         // changes motor_state variable to true
+	void turn_off();        // changes motor_state variable to false
+	long posi{ 0 };           // position of rotary encoder \ number of pulses
 	void set_position(float posi = 0);
 	long get_position();
 	void set_target(float);
@@ -29,15 +29,15 @@ public:
 	bool target_reached(bool reset = false); // check if target position is reached by motor
 
 private:
-	uint8_t ENCA, ENCB, IN2, IN1;
-	void setMotor(int dir, int pwmVal);
-	void readEncoder();
-	void RisingInterrupt(); // function for detecting rising edge
-	bool buffer{false};
-	uint8_t _pwmpin = 0;
-	int _upper_limit = 0, _lower_limit = 0;
-	long prevT{0};
-	long target{0};
+	uint8_t enca, encb, in2, in1;
+	void set_motor(int dir, int pwmVal);
+	void read_encoder();
+	void rising_interrupt(); // function for detecting rising edge
+	bool buffer{ false };
+	uint8_t pwmpin = 0;
+	int upper_limit = 0, lower_limit = 0;
+	long prev_t{ 0 };
+	long target{ 0 };
 	float eprev = 0, eintegral = 0;
 	bool target_is_reached = false;
 };
